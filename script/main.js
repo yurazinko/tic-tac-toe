abd2bde340994afc4c0bffa9761b0ab86de9445b
Yurii Zinko <yurazinko@outlook.com>
2016-08-04 18:04:11 +0300

    fix cell rewriting

--- script/main.js
+++ script/main.js
@@ -12,16 +12,52 @@ $(function(){
 	];
 	$('.cell').click(function() {
 		this.innerHTML = '<p>X</p>';
-		setTimeout(function(){
-			for (var i = 0; i < cellsArray.length; i++) {
-				if (cellsArray[i].innerHTML !== '<p>X</p>' && cellsArray[i] !== '<p>0</p>') {
-					cellsArray[Math.floor((Math.random() * 8) + 1)].innerHTML = '<p>0</p>';
-					break;
+		
+		function compTurn() {
+			var random = (function() { return Math.floor((Math.random() * 8) + 1)})();
+				console.log('Random: ' + random);
+				if (cellsArray[random].innerHTML == '<p>X</p>' || cellsArray[random].innerHTML == '<p>0</p>') {
+					compTurn();
 				} else {
-					i++; 
+					cellsArray[random].innerHTML = '<p>0</p>'; 
 				}
-			}	
-		}, 2000);
+
+				if        (cell01.innerHTML == '<p>X</p>' && cell02.innerHTML == '<p>X</p>' && cell03.innerHTML == '<p>X</p>') { // X
+					alert('x wins');
+				} else if (cell01.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
+					alert('x wins');
+				} else if (cell01.innerHTML == '<p>X</p>' && cell04.innerHTML == '<p>X</p>' && cell07.innerHTML == '<p>X</p>') {
+					alert('x wins');
+				} else if (cell02.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell08.innerHTML == '<p>X</p>') {
+					alert('x wins');
+				} else if (cell03.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell07.innerHTML == '<p>X</p>') {
+					alert('x wins');
+				} else if (cell03.innerHTML == '<p>X</p>' && cell06.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
+					alert('x wins');
+				} else if (cell04.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell06.innerHTML == '<p>X</p>') {
+					alert('x wins');
+				} else if (cell07.innerHTML == '<p>X</p>' && cell08.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
+					alert('x wins'); 
+				} else if (cell01.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') { // 0
+					alert('0 wins');
+				} else if (cell01.innerHTML == '<p>0</p>' && cell04.innerHTML == '<p>0</p>' && cell07.innerHTML == '<p>0</p>') {
+					alert('0 wins');
+				} else if (cell02.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell08.innerHTML == '<p>0</p>') {
+					alert('0 wins');
+				} else if (cell03.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell07.innerHTML == '<p>0</p>') {
+					alert('0 wins');
+				} else if (cell03.innerHTML == '<p>0</p>' && cell06.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') {
+					alert('0 wins');
+				} else if (cell04.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell06.innerHTML == '<p>0</p>') {
+					alert('0 wins');
+				} else if (cell07.innerHTML == '<p>0</p>' && cell08.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') {
+					alert('0 wins');
+				}
+
+
+				return;
+		}
+
+		setTimeout(compTurn, 500);
 	})
 })
-
