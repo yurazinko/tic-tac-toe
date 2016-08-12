1674187c02a28f73496a3a9013e54118c414102a
Yurii Zinko <yurazinko@outlook.com>
2016-08-12 17:49:49 +0300

    functions arrange

--- script/main.js
+++ script/main.js
@@ -1,4 +1,5 @@
 $(function() {
+    
     var cellsArray = [
         cell01,
         cell02,
@@ -28,9 +29,58 @@ $(function() {
         }
     }
 
+    function checkCombination() {
+        if (cell01.innerHTML == '<p>X</p>' && cell02.innerHTML == '<p>X</p>' && cell03.innerHTML == '<p>X</p>') { // X
+            alert('x wins');
+            clearCells();
+        } else if (cell01.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
+            alert('x wins');
+            clearCells();
+        } else if (cell01.innerHTML == '<p>X</p>' && cell04.innerHTML == '<p>X</p>' && cell07.innerHTML == '<p>X</p>') {
+            alert('x wins');
+            clearCells();
+        } else if (cell02.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell08.innerHTML == '<p>X</p>') {
+            alert('x wins');
+            clearCells();
+        } else if (cell03.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell07.innerHTML == '<p>X</p>') {
+            alert('x wins');
+            clearCells();
+        } else if (cell03.innerHTML == '<p>X</p>' && cell06.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
+            alert('x wins');
+            clearCells();
+        } else if (cell04.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell06.innerHTML == '<p>X</p>') {
+            alert('x wins');
+            clearCells();
+        } else if (cell07.innerHTML == '<p>X</p>' && cell08.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
+            alert('x wins');
+            clearCells();
+        } else if (cell01.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') { // 0
+            alert('0 wins');
+            clearCells();
+        } else if (cell01.innerHTML == '<p>0</p>' && cell04.innerHTML == '<p>0</p>' && cell07.innerHTML == '<p>0</p>') {
+            alert('0 wins');
+            clearCells();
+        } else if (cell02.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell08.innerHTML == '<p>0</p>') {
+            alert('0 wins');
+            clearCells();
+        } else if (cell03.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell07.innerHTML == '<p>0</p>') {
+            alert('0 wins');
+            clearCells();
+        } else if (cell03.innerHTML == '<p>0</p>' && cell06.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') {
+            alert('0 wins');
+            clearCells();
+        } else if (cell04.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell06.innerHTML == '<p>0</p>') {
+            alert('0 wins');
+            clearCells();
+        } else if (cell07.innerHTML == '<p>0</p>' && cell08.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') {
+            alert('0 wins');
+            clearCells();
+        }
+    }
+
     $('.cell').click(function() {
         function compTurn() {
-            
+
             var random = (function() {
                 if (count < 5) {
                     return Math.floor((Math.random() * 8) + 1);
@@ -48,52 +98,7 @@ $(function() {
                 cellsArray[random].innerHTML = '<p>0</p>';
             }
 
-            if (cell01.innerHTML == '<p>X</p>' && cell02.innerHTML == '<p>X</p>' && cell03.innerHTML == '<p>X</p>') { // X
-                alert('x wins');
-                clearCells();
-            } else if (cell01.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
-                alert('x wins');
-                clearCells();
-            } else if (cell01.innerHTML == '<p>X</p>' && cell04.innerHTML == '<p>X</p>' && cell07.innerHTML == '<p>X</p>') {
-                alert('x wins');
-                clearCells();
-            } else if (cell02.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell08.innerHTML == '<p>X</p>') {
-                alert('x wins');
-                clearCells();
-            } else if (cell03.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell07.innerHTML == '<p>X</p>') {
-                alert('x wins');
-                clearCells();
-            } else if (cell03.innerHTML == '<p>X</p>' && cell06.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
-                alert('x wins');
-                clearCells();
-            } else if (cell04.innerHTML == '<p>X</p>' && cell05.innerHTML == '<p>X</p>' && cell06.innerHTML == '<p>X</p>') {
-                alert('x wins');
-                clearCells();
-            } else if (cell07.innerHTML == '<p>X</p>' && cell08.innerHTML == '<p>X</p>' && cell09.innerHTML == '<p>X</p>') {
-                alert('x wins');
-                clearCells();
-            } else if (cell01.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') { // 0
-                alert('0 wins');
-                clearCells();
-            } else if (cell01.innerHTML == '<p>0</p>' && cell04.innerHTML == '<p>0</p>' && cell07.innerHTML == '<p>0</p>') {
-                alert('0 wins');
-                clearCells();
-            } else if (cell02.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell08.innerHTML == '<p>0</p>') {
-                alert('0 wins');
-                clearCells();
-            } else if (cell03.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell07.innerHTML == '<p>0</p>') {
-                alert('0 wins');
-                clearCells();
-            } else if (cell03.innerHTML == '<p>0</p>' && cell06.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') {
-                alert('0 wins');
-                clearCells();
-            } else if (cell04.innerHTML == '<p>0</p>' && cell05.innerHTML == '<p>0</p>' && cell06.innerHTML == '<p>0</p>') {
-                alert('0 wins');
-                clearCells();
-            } else if (cell07.innerHTML == '<p>0</p>' && cell08.innerHTML == '<p>0</p>' && cell09.innerHTML == '<p>0</p>') {
-                alert('0 wins');
-                clearCells();
-            }
+            checkCombination();
         }
 
         if (this.innerHTML == '<p>X</p>' || this.innerHTML == '<p>0</p>') {
